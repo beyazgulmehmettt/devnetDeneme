7c287106c3cbc58cf2dd03660f1f9651185ad6a5
NetAcad DEVASC <changeme@example.com>
2021-07-08 16:43:41 +0000

    ugurlama metodu eklendi

--- kisi.py
+++ kisi.py
@@ -4,6 +4,10 @@ class kisi:
     self.soyad=soyad
   def karsilama(self):
     print("hoş geldin, ",self.ad)
+  def ugurlama(self):
+    print("güle güle, ",self.ad)
+   
 
 kisi1=kisi("mehmet",34)
 kisi1.karsilama()
+kisi1.ugurlama()
