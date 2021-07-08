c7e786157b8895a4bb600fb8df18c4c517243493
NetAcad DEVASC <changeme@example.com>
2021-07-08 20:37:26 +0000

    sayfa ekklendi

--- kisi.py
+++ kisi.py
@@ -1,13 +1,15 @@
 class kisi:
-  def __init__(self,ad,soyad):
+  def __init__(self,ad,soyad,yas):
     self.ad=ad
     self.soyad=soyad
+    self.yas=yas
   def karsilama(self):
-    print("hoş geldin, ",self.ad)
+    print("hoş geldin, ",self.ad,self.soyad)
+    print("yaşın: ",self.yas)
   def ugurlama(self):
     print("güle güle, ",self.ad)
    
 
-kisi1=kisi("mehmet",34)
+kisi1=kisi("mehmet","beyazgül",34)
 kisi1.karsilama()
 kisi1.ugurlama()
